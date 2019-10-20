3c0efd978248c613b41c9a6d04fee39a37ef7cb7
Gerardas Martynovas <gerardas@8devices.com>
2019-10-20 22:55:37 +0300

    correct arithmetic coding

--- calculations/ArithmeticCoding.py
+++ calculations/ArithmeticCoding.py
@@ -1,8 +1,22 @@
 #!/usr/bin/python3
 
 import fractions
+import re
+import progressbar
 
 from ExtractFromPdf import ExtractFromPdf
+
+
+def calc_numerator(message) -> str:
+    return re.sub(r'10*', '', message, 1)
+
+def calc_denominator(message) -> str:
+    output = '0b1'
+    bits = len(message) - 2
+    for bit in range(bits - 1):
+        output += '0'
+
+    return output
 
 
 class ArithmeticCoding:
@@ -20,63 +34,82 @@ class ArithmeticCoding:
             start_interval = start_interval + self.width[letter]
 
     def encode(self, message, message_length) -> str:
+        print('Encoding message')
+        bar = progressbar.ProgressBar(
+            maxval=100,
+            widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()]
+        )
+        bar.start()
+        bar_counter = 0
+        bar_increment = 100 / message_length
+
         start = fractions.Fraction(0, message_length)
         width = fractions.Fraction(message_length, message_length)
 
         for letter in message:
+            bar_counter += bar_increment
+            bar.update(bar_counter)
             start += self.interval_start[letter] * width
             width *= self.width[letter]
 
         end = start + width
 
-        bin_fraction = fractions.Fraction()
-        power = 1
+        fraction = fractions.Fraction()
+        denominator = 1
         while True:
-            if start <= bin_fraction < end:
+            if start <= fraction < end:
                 break
 
-            numerator = (start.numerator * power) // start.denominator + 1
-            bin_fraction = fractions.Fraction(numerator, power)
+            numerator = ((start.numerator * denominator) // start.denominator) + 1
+            fraction = fractions.Fraction(numerator, denominator)
 
-            power *= 2
+            denominator *= 2
 
-        numerator_binary = bin(bin_fraction.numerator)
+        fraction_xor = fraction.numerator ^ fraction.denominator
+        numerator_binary = bin(fraction_xor)
+        bar.finish()
         return numerator_binary
 
     def decode(self, message, message_length) -> str:
-        numerator = int(message, 2)
-        denominator_bin = '0b1' + '0' * (len(message) - 2)
-        denominator = int(denominator_bin, 2)
+        print('Decoding message')
+        bar = progressbar.ProgressBar(
+            maxval=100,
+            widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()]
+        )
+        bar.start()
+        bar_counter = 0
+        bar_increment = 100 / message_length
+
+        numerator_adjusted = calc_numerator(message)
+        denominator_adjusted = calc_denominator(message)
+        numerator = int(numerator_adjusted, 2)
+        denominator = int(denominator_adjusted, 2)
         input_fraction = fractions.Fraction(numerator, denominator)
         output = ''
 
         while len(output) < message_length:
+            bar_counter += bar_increment
+            bar.update(bar_counter)
             for letter in self.interval_start:
                 if 0 <= input_fraction - self.interval_start[letter] < self.width[letter]:
                     input_fraction = (input_fraction - self.interval_start[letter]) / self.width[letter]
                     output += letter
 
+        bar.finish()
         return output
 
 
 def main():
-    # extractor = ExtractFromPdf('Neris.pdf')
-    # dictionary = extractor.get_letter_dictionary()
-    # alphabet = extractor.get_alphabet()
-    # total_symbols = extractor.get_length()
-
-    dictionary = {
-        '1': 1,
-        '2': 1,
-        '3': 2,
-    }
-    alphabet = ['1', '2', '3']
-    text = '3123'
+    extractor = ExtractFromPdf('Neris.pdf')
+    dictionary = extractor.get_letter_dictionary()
+    total_symbols = extractor.get_length()
+    text = extractor.get_words()
     coding = ArithmeticCoding()
-    coding.calc_intervals(dictionary, len(text))
-    encoded_message = coding.encode(text, len(text))
+    coding.calc_intervals(dictionary, total_symbols)
+    print(text)
+    encoded_message = coding.encode(text, total_symbols)
     print(encoded_message)
-    decoded_message = coding.decode(encoded_message, len(text))
+    decoded_message = coding.decode(encoded_message, total_symbols)
     print(decoded_message)
 
 
