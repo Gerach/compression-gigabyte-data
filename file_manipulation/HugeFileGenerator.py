1f1e7410d59b6331b33edd46fba0c70bf02b13a4
Gerardas Martynovas <gerardas@8devices.com>
2019-12-12 01:12:19 +0200

    Implement huge text file generator

---
+++ file_manipulation/HugeFileGenerator.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+import random
+
+from multiprocessing import Pool
+
+
+class HugeFileGenerator:
+    def __init__(self):
+        self.words = []
+        self.weights = []
+
+        with open('words.csv', 'r') as rf:
+            lines = rf.readlines()
+
+        total_freq = 0
+        for line in lines:
+            word, freq = line.split(',')
+            total_freq += int(freq)
+
+        for line in lines:
+            word, freq = line.split(',')
+            self.words.append(str.strip(word, '"'))
+            self.weights.append(int(freq) / total_freq)
+
+    def get_random_word(self, whatever):
+        return random.choices(self.words, self.weights)[0]
+
+    def generateFile(self, filename, size=1):
+        """
+        Size given in megabytes
+        """
+        size_bytes = size * 1024 * 1024
+        text = ''
+        pool = Pool(2)
+        while len(text) * 8 < size_bytes:
+            text += ' '.join(pool.map(self.get_random_word, range(2048)))
+
+        with open(filename, 'w') as wf:
+            wf.write(text)
+
+
+def main():
+    generator = HugeFileGenerator()
+    generator.generateFile('random.txt', 1024)
+
+
+if __name__ == "__main__":
+    main()
