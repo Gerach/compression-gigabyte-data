cc77778563dbb5fecf8599e57b09801ab0898d1c
Gerardas Martynovas <gerardas@8devices.com>
2019-12-15 18:18:40 +0200

    Upgrade huge file generator to be more realistic

--- file_manipulation/HugeFileGenerator.py
+++ file_manipulation/HugeFileGenerator.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import random
+import time
 
 from multiprocessing import Pool
 
@@ -24,25 +25,36 @@ class HugeFileGenerator:
             self.weights.append(int(freq) / total_freq)
 
     def get_random_word(self, whatever):
-        return random.choices(self.words, self.weights)[0]
+        random_separator = random.randint(1, 1000)
+        if random_separator < 5:
+            return random.choices(self.words, self.weights)[0] + '.\n'\
+                   + random.choices(self.words, self.weights)[0].capitalize()
+        elif random_separator < 100:
+            return random.choices(self.words, self.weights)[0] + '. '\
+                   + random.choices(self.words, self.weights)[0].capitalize()
+        else:
+            return random.choices(self.words, self.weights)[0] + ' '
 
-    def generateFile(self, filename, size=1):
+    def generate_file(self, filename, size=1.0):
         """
         Size given in megabytes
         """
         size_bytes = size * 1024 * 1024
         text = ''
         pool = Pool(2)
-        while len(text) * 8 < size_bytes:
-            text += ' '.join(pool.map(self.get_random_word, range(2048)))
+        start_time = time.time()
+        while len(text) < size_bytes:
+            text += ''.join(pool.map(self.get_random_word, range(2048)))
 
         with open(filename, 'w', encoding='utf8') as wf:
             wf.write(text)
 
+        print(time.time() - start_time)
+
 
 def main():
     generator = HugeFileGenerator()
-    generator.generateFile('random.txt', 1024)
+    generator.generate_file('random.txt', 0.1)
 
 
 if __name__ == "__main__":
